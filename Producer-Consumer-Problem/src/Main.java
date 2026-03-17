a80968e32e396cc407a201fddc836db89f193605
Divya <divyaakshay011@gmail.com>
2026-03-17 22:52:00 +0530

    updated gitignore

--- Producer-Consumer-Problem/src/Main.java
+++ Producer-Consumer-Problem/src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    System.out.println("data");
+    System.out.println("data added");
     }
 }
